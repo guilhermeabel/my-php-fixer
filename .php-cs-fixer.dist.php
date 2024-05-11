3f5ef7cc1d707d8cddcaee1512a84bf359284605
Guilherme Abel <guilhermeabel8@gmail.com>
2024-05-11 16:32:10 -0300

    add: personal config

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -24,19 +24,18 @@ return (new Config())
         '@PhpCsFixer' => true,
         '@PhpCsFixer:risky' => true,
         'general_phpdoc_annotation_remove' => ['annotations' => ['expectedDeprecation']], // one should use PHPUnit built-in method instead
-        'header_comment' => ['header' => <<<'EOF'
-            This file is part of PHP CS Fixer.
-
-            (c) Fabien Potencier <fabien@symfony.com>
-                Dariusz Rumiński <dariusz.ruminski@gmail.com>
-
-            This source file is subject to the MIT license that is bundled
-            with this source code in the file LICENSE.
-            EOF],
         'modernize_strpos' => true, // needs PHP 8+ or polyfill
         'no_useless_concat_operator' => false, // TODO switch back on when the `src/Console/Application.php` no longer needs the concat
         'numeric_literal_separator' => true,
-        'string_implicit_backslashes' => true, // https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/pull/7786
+        'string_implicit_backslashes' => true, // https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/pull/7786,
+        'braces_position' => [
+            'allow_single_line_anonymous_functions' => true,
+            'allow_single_line_empty_anonymous_classes' => true,
+            'functions_opening_brace' => 'same_line',
+            'classes_opening_brace' => 'same_line',
+        ],
+        'concat_space' => ['spacing' => 'one'],
+        'declare_strict_types' => true,
     ])
     ->setFinder(
         (new Finder())
